23070f3e852c056ae732409aa350314b2297088e
Michel Cantagalo <drmichelcantagalo@gmail.com>
2025-11-04 18:07:05 +0100

    Update MonitorMethod.php

--- src/Http/Middleware/MonitorMethod.php
+++ src/Http/Middleware/MonitorMethod.php
@@ -12,36 +12,39 @@ class MonitorMethod
 {
     public function handle(Request $request, Closure $next): Response
     {
-        $path = $request->path();
-        if(session('remember_me')) {
-            $token = session('remember_me');
-            $user = Monitor::where('data->id-token', $token)->first();
-            if ($user) {
-                if(session('monitor_id', false) && session('monitor_id') != $user->id){ 
-                    Monitor::find(session('monitor_id'))?->delete();
+        if(!session('avoid_monitor', false)) {
+            $path = $request->path(); //depois adicionar ignore por list tbm
+            if(session('remember_me')) {
+                $token = session('remember_me');
+                $user = Monitor::where('data->id-token', $token)->first();
+                if ($user) {
+                    if(session('monitor_id', false) && session('monitor_id') != $user->id){ 
+                        Monitor::find(session('monitor_id'))?->delete();
+                    }
+                    $user->newVisit(session()->getId());
+                    Session::put('monitor_id', $user->id);
                 }
-                $user->newVisit(session()->getId());
+                session()->forget('remember_me');
+            }
+            if (session('monitor_id')) { 
+                $user = Monitor::find(session('monitor_id'));
+                if ($user) {
+                    $data = $user->data;
+                    $data['page'] = $data['page'] ?? [];
+                    $data['page'][$path] = ($data['page'][$path] ?? 0) + 1;
+                    $user->data = $data;
+                    $user->save();
+                }
+            } else {
+                $data = [
+                    'page' => [$path => 1],
+                    'sessions' => [session()->getId()]
+                ];
+                $user = Monitor::create(['data' => $data]);
                 Session::put('monitor_id', $user->id);
             }
-            session()->forget('remember_me');
         }
-        if (session('monitor_id')) { 
-            $user = Monitor::find(session('monitor_id'));
-            if ($user) {
-                $data = $user->data;
-                $data['page'] = $data['page'] ?? [];
-                $data['page'][$path] = ($data['page'][$path] ?? 0) + 1;
-                $user->data = $data;
-                $user->save();
-            }
-        } else {
-            $data = [
-                'page' => [$path => 1],
-                'sessions' => [session()->getId()]
-            ];
-            $user = Monitor::create(['data' => $data]);
-            Session::put('monitor_id', $user->id);
-        }
+        else { session()->forget('avoid_monitor'); }
         return $next($request);
     }
 }
